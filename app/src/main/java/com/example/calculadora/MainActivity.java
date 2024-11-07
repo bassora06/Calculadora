4f12f2fe73cc7e5877266e719e3961df6b138e31
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-11-06 23:22:41 -0300

    nova calculadora atualizada

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -17,12 +17,12 @@ import androidx.core.view.WindowInsetsCompat;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
 
-    private Button number1, number2, number3, number4, number5, number6, number7, number8, number9, number0, dividir, limpar,
-            somar, subtrair, igual, apagar, virgula, multiplicar;
+public class MainActivity extends AppCompatActivity {
 
-    private TextView txtResultado, txtEspressao;
+    double primeiroNumero;
+    String operacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,136 +35,114 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return insets;
         });
 
-        iniciarComponentes();
+        Button btn0 = findViewById(R.id.btn0);
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+        Button btn5 = findViewById(R.id.btn5);
+        Button btn6 = findViewById(R.id.btn6);
+        Button btn7 = findViewById(R.id.btn7);
+        Button btn8 = findViewById(R.id.btn8);
+        Button btn9 = findViewById(R.id.btn9);
+        Button btnSomar = findViewById(R.id.btnSomar);
+        Button btnSubtrair = findViewById(R.id.btnSubtrair);
+        Button btnMultiplicar = findViewById(R.id.btnMultiplicar);
+        Button btnDividir = findViewById(R.id.btnDividir);
+        Button btnIgual = findViewById(R.id.btnIgual);
+        Button btnPonto = findViewById(R.id.btnPonto);
+        Button btnOff = findViewById(R.id.btnOff);
+        Button btnOn = findViewById(R.id.btnOn);
+        Button btnAc = findViewById(R.id.btnAc);
+        Button btnDeletar = findViewById(R.id.btnDeletar);
 
-        number0.setOnClickListener(this);
-        number1.setOnClickListener(this);
-        number2.setOnClickListener(this);
-        number3.setOnClickListener(this);
-        number4.setOnClickListener(this);
-        number5.setOnClickListener(this);
-        number6.setOnClickListener(this);
-        number7.setOnClickListener(this);
-        number8.setOnClickListener(this);
-        number9.setOnClickListener(this);
-        somar.setOnClickListener(this);
-        dividir.setOnClickListener(this);
-        multiplicar.setOnClickListener(this);
-        subtrair.setOnClickListener(this);
-        virgula.setOnClickListener(this);
+        TextView visor = findViewById(R.id.txtVisor);
 
-        limpar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txtEspressao.setText("");
-                txtResultado.setText("");
-            }
+        btnOff.setOnClickListener(view -> visor.setVisibility(View.INVISIBLE));
+        btnOn.setOnClickListener(view -> {
+                    visor.setVisibility(View.VISIBLE);
+                    visor.setText("0");
+                });
+
+        btnAc.setOnClickListener(view -> {
+            primeiroNumero = 0;
+            visor.setText("0");
         });
 
-        apagar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView expressao = findViewById(R.id.txtEspressao);
-                String string = expressao.getText().toString();
+        ArrayList<Button> nums = new ArrayList<>();
+        nums.add(btn1);
+        nums.add(btn2);
+        nums.add(btn3);
+        nums.add(btn4);
+        nums.add(btn5);
+        nums.add(btn6);
+        nums.add(btn7);
+        nums.add(btn8);
+        nums.add(btn9);
+        nums.add(btn0);
 
-                if(!string.isEmpty()){
-                    byte var0 = 0;
-                    int var1 = string.length()-1;
-                    String txtExpressao = string.substring(var0, var1);
-                    expressao.setText(txtExpressao);
-                }
-                txtResultado.setText("");
-            }
-        });
-
-        igual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Expression expressao = new ExpressionBuilder(txtEspressao.getText().toString()).build();
-                double resultado = expressao.evaluate();
-                long longResult = (long) resultado;
-
-                if(resultado == (double)longResult){
-                    txtResultado.setText((CharSequence) String.valueOf(longResult));
+        for (Button b: nums){
+            b.setOnClickListener(view -> {
+                if (!visor.getText().toString().equals("0")){
+                    visor.setText(visor.getText().toString() + b.getText().toString());
                 }else{
-                    txtResultado.setText((CharSequence) String.valueOf(resultado));
+                    visor.setText(b.getText().toString());
                 }
+            });
+        }
+
+        ArrayList<Button> operadores = new ArrayList<>();
+        operadores.add(btnSomar);
+        operadores.add(btnSubtrair);
+        operadores.add(btnMultiplicar);
+        operadores.add(btnDividir);
+
+        for (Button b: operadores){
+            b.setOnClickListener(view -> {
+                primeiroNumero = Double.parseDouble(visor.getText().toString());
+                operacao = b.getText().toString();
+
+                visor.setText("0");
+
+            });
+        }
+
+        btnDeletar.setOnClickListener(view ->{
+            String num = visor.getText().toString();
+            if (num.length() > 1){
+                visor.setText(num.substring(0, num.length()-1));
+            }else if(num.length() == 1 && !num.equals("0")){
+                visor.setText("0");
             }
         });
-    }
 
-    private void iniciarComponentes(){
-        number0 = findViewById(R.id.btn0);
-        number1 = findViewById(R.id.btn1);
-        number2 = findViewById(R.id.btn2);
-        number3 = findViewById(R.id.btn3);
-        number4 = findViewById(R.id.btn4);
-        number5 = findViewById(R.id.btn5);
-        number6 = findViewById(R.id.btn6);
-        number7 = findViewById(R.id.btn7);
-        number8 = findViewById(R.id.btn8);
-        number9 = findViewById(R.id.btn9);
-        somar = findViewById(R.id.btnSomar);
-        dividir = findViewById(R.id.btnDividir);
-        multiplicar = findViewById(R.id.btnMultiplicar);
-        subtrair = findViewById(R.id.btnSubtrair);
-        limpar = findViewById(R.id.btnC);
-        apagar = findViewById(R.id.btnApagar);
-        igual = findViewById(R.id.btnIgual);
-        virgula = findViewById(R.id.btnVirgula);
-        txtEspressao = findViewById(R.id.txtEspressao);
-        txtResultado = findViewById(R.id.txtResultado);
-
-    }
-
-    public void acrescrentarUmaExpressao(String string,boolean limparDados){
-        if(txtResultado.getText().equals("")){
-            txtEspressao.setText(" ");
-        }
-
-        if(limparDados){
-            txtResultado.setText(" ");
-            txtEspressao.append(string);
-
-        }else{
-            txtEspressao.append(txtResultado.getText());
-            txtEspressao.append(string);
-            txtResultado.setText(" ");
-        }
-    }
-
-    @Override
-    public void onClick(View view) {
-            if(view.getId() == R.id.btn0){
-                acrescrentarUmaExpressao("0", true);
-            }else if(view.getId() == R.id.btn1){
-                acrescrentarUmaExpressao("1", true);
-            }else if (view.getId() == R.id.btn2){
-                acrescrentarUmaExpressao("2", true);
-            }else if (view.getId() == R.id.btn3){
-                acrescrentarUmaExpressao("3", true);
-            }else if (view.getId() == R.id.btn4){
-                acrescrentarUmaExpressao("4", true);
-            }else if (view.getId() == R.id.btn5){
-                acrescrentarUmaExpressao("5", true);
-            }else if (view.getId() == R.id.btn6){
-                acrescrentarUmaExpressao("6", true);
-            }else if (view.getId() == R.id.btn7){
-                acrescrentarUmaExpressao("7", true);
-            }else if (view.getId() == R.id.btn8){
-                acrescrentarUmaExpressao("8", true);
-            }else if (view.getId() == R.id.btn9){
-                acrescrentarUmaExpressao("9", true);
-            }else if (view.getId() == R.id.btnVirgula){
-                acrescrentarUmaExpressao(".", true);
-            }else if (view.getId() == R.id.btnSomar){
-                acrescrentarUmaExpressao("+", false);
-            }else if (view.getId() == R.id.btnSubtrair){
-                acrescrentarUmaExpressao("-", false);
-            }else if (view.getId() == R.id.btnMultiplicar){
-                acrescrentarUmaExpressao("*", false);
-            }else if (view.getId() == R.id.btnDividir){
-                acrescrentarUmaExpressao("/", false);
+        btnPonto.setOnClickListener(view -> {
+            if (!visor.getText().toString().contains(".")){
+                visor.setText(visor.getText().toString() + ".");
             }
+        });
+
+        btnIgual.setOnClickListener(view -> {
+            double segundoNumero = Double.parseDouble(visor.getText().toString());
+            double resultado;
+            switch (operacao){
+                case "/":
+                    resultado = primeiroNumero / segundoNumero;
+                    break;
+                case "X":
+                    resultado = primeiroNumero * segundoNumero;
+                    break;
+                case "+":
+                    resultado = primeiroNumero+ segundoNumero;
+                    break;
+                case "-":
+                    resultado = primeiroNumero - segundoNumero;
+                    break;
+                default:
+                    resultado = primeiroNumero + segundoNumero;
+            }
+            visor.setText(String.valueOf(resultado));
+            primeiroNumero = resultado;
+        });
     }
 }
